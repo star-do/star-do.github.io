42b3e9bb4d3c5d1ce832f1f0a64ff85d0ac1d48f
star-do <dhxtry@gmail.com>
2024-02-21 11:43:22 +0800

    Site updated: 2024-02-21 11:43:20

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/02/21/hello-world/"];function toRandomPost(){
+var posts=["2024/02/21/hello-world/","2024/02/21/如何创建一个hexo的静态博客-1/","2024/02/21/模板/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
